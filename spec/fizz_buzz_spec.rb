a7645697d5c34874c9b76ec4b00a3a51ba136ff8
tochman <thomas@nocebo.se>
2015-09-01 14:05:25 +0200

    new changes to fizz_buzz

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,5 +1,20 @@
 require "fizz_buzz.rb"
 
 describe "fizz_buzz" do 
+  it "if number is % 3" do
+    expect(new_fizz_buzz(3)).to eq "Fizz"
+    
+  end 
+  
+  it "if number is % 5" do
+    expect(new_fizz_buzz(5)).to eq "Buzz"
+    
+  end 
+  
+  it "if number is % 15" do
+    expect(new_fizz_buzz(15)).to eq "FizzBuzz"
+    
+  end
+  
   
 end
